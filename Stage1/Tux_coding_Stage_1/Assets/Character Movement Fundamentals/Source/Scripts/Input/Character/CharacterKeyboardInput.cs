a9841886527b7b40c3c9cc0ace5b455da72368b8
capitofc <capitofernan101420@gmail.com>
2021-07-20 16:21:53 +0800

    Design the stage and added joystick controller

---
+++ Stage1/Tux_coding_Stage_1/Assets/Character Movement Fundamentals/Source/Scripts/Input/Character/CharacterKeyboardInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CMF
+{
+	//This character movement input class is an example of how to get input from a keyboard to control the character;
+    public class CharacterKeyboardInput : CharacterInput
+    {
+		public string horizontalInputAxis = "Horizontal";
+		public string verticalInputAxis = "Vertical";
+		public KeyCode jumpKey = KeyCode.Space;
+		public FixedButton Button;
+		public FixedJoystick rightJoyStick;
+
+		//If this is enabled, Unity's internal input smoothing is bypassed;
+		public bool useRawInput = true;
+
+        public override float GetHorizontalMovementInput()
+		{
+			if(useRawInput)
+				return rightJoyStick.Direction.x;
+			else
+				return rightJoyStick.Direction.x;
+		}
+
+		public override float GetVerticalMovementInput()
+		{
+			if(useRawInput)
+				return rightJoyStick.Direction.y;
+			else
+				return rightJoyStick.Direction.y;
+		}
+
+		public override bool IsJumpKeyPressed()
+		{
+			return Button.Pressed;
+		}
+
+		
+    }
+}
---
+++ Stage1/Tux_coding_Stage_1/Assets/Script/stage_script.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stage_script : MonoBehaviour
+{
+    public string value = "null";
+    
+
+}
